615e12eede0b614eb93947c016ebcf1b5dc43c5d
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-05-11 22:31:46 -0300

    Post

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,23 @@
+package com.api.parkingcontrol.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+	
+	
+	final ParkingSpotRepository parkingSpotRepository;
+	
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+	
+	@Transactional
+	public ParkingSpotModel saveVaga(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+}
